052330c949816a4fd576cada8013731ac3f9f75a
Stephen Sanchez <stephensanchez05@gmail.com>
2019-11-15 02:42:25 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,15 +1,11 @@
-def my_collect(collection)
+def my_collect(array)
   i = 0
  
-  while i < collection.length
-  yield(collection[i])
+  while i < array.length
+  yield(array[i])
   i = i + 1
 end
-collection
+array
 end
 
-def my_collect(array) 
-  do |name|
-  name.split(" ").first
-end
 
