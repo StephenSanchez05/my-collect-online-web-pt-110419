2378862f953346a988ae3555f707024da21481ed
Stephen Sanchez <stephensanchez05@gmail.com>
2019-11-15 02:41:58 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,13 @@
+def my_each(collection)
+  i = 0
+ 
+  while i < collection.length
+  yield(collection[i])
+  i = i + 1
+end
+collection
+end
+
 def my_collect(array) 
   do |name|
   name.split(" ").first
